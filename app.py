b88e79eb1466102b8123da3a4aea5fe131a925dd
n1ght0wl <a.charala@outlook.com>
2023-05-18 02:46:12 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, jsonify
-#from database import engine
+from database import load_jobs_from_db, load_job_from_db
 #from sqlalchemy import text
-
-from database import load_jobs_from_db
 
 app = Flask(__name__)
 
@@ -27,17 +25,26 @@ app = Flask(__name__)
 #   'salary': '$150, 000'
 # }]
 
-
 @app.route("/")
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template("home.html", jobs=jobs, company_name='Jovian')
+  return render_template("home.html", jobs=jobs)
 
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+
+  if not job:
+    return "Not found", 404
+  #return jsonify(job)
+  return render_template("jobpage.html", job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -49,3 +49,38 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(zip(column_names, row)))
     return jobs
+
+
+# def load_job_from_db(id):
+#   with engine.connect() as conn:
+#     result = conn.execute(text("SELECT * FROM jobs WHERE id :val"), val=id)
+
+# rows = result.all()
+# if len(rows) == 0:
+#   return None
+# else:
+#   return dict(rows[0])
+
+# def load_job_from_db(id):
+#   with engine.connect() as conn:
+#     result = conn.execute(text(f"SELECT * FROM jobs WHERE id={id}"))
+#     rows = []
+#     for row in result.all():
+#       rows.append(row._mapping)
+#     if len(rows) == 0:
+#       return None
+#     else:
+#       return [dict(row) for row in rows]
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text(f"SELECT * FROM jobs WHERE id = :val"),
+      {"val": id}
+    )
+    rows = result.mappings().all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
